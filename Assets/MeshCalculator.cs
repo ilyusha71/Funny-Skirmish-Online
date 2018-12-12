06426d9c1d209b06f39173ccdc81fd7c4335401b
iLYuSha <ilyusha71@gmail.com>
2018-12-12 19:15:40 +0800

    2018-12-12 下班

---
+++ Assets/MeshCalculator.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(MeshFilter))]
+public class MeshCalculator : MonoBehaviour
+{
+    private MeshFilter mf = null;
+    private Vector3 scale;
+    private float sum;
+    private string sum1;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.mf = this.GetComponent<MeshFilter>();
+        //有损缩放
+        this.scale = this.transform.lossyScale;
+        this.CalculateSumArea();
+        this.CalculateSumVolume();
+    }
+    private void CalculateSumVolume()
+    {
+        Vector3[] arrVertices = this.mf.mesh.vertices;
+        int[] arrTriangles = this.mf.mesh.triangles;
+        float sum = 0.0f;
+        for (int i = 0; i < this.mf.mesh.subMeshCount; i++)
+        {
+            int[] arrIndices = this.mf.mesh.GetTriangles(i);
+            for (int j = 0; j < arrIndices.Length; j += 3)
+                sum += this.CalculateVolume(arrVertices[arrIndices[j]]
+                                        , arrVertices[arrIndices[j + 1]]
+                                        , arrVertices[arrIndices[j + 2]]);
+        }
+
+
+        Debug.Log("Volume= " + Mathf.Abs(sum));
+    }
+    private void CalculateSumArea()
+    {
+        Vector3[] arrVertices = this.mf.mesh.vertices;
+        int[] arrTriangles = this.mf.mesh.triangles;
+        float sum1 = 0.0f;
+        for (int i = 0; i < this.mf.mesh.subMeshCount; i++)
+        {
+            int[] arrIndices = this.mf.mesh.GetTriangles(i);
+            for (int j = 0; j < arrIndices.Length; j += 3)
+                sum1 += this.CalculateArea(arrVertices[arrIndices[j]]
+                                        , arrVertices[arrIndices[j + 1]]
+                                        , arrVertices[arrIndices[j + 2]]);
+        }
+
+        Debug.Log("Area = " + sum1);
+    }
+    private float CalculateVolume(Vector3 pt0, Vector3 pt1, Vector3 pt2)
+    {
+        pt0 = new Vector3(pt0.x * this.scale.x, pt0.y * this.scale.y, pt0.z * this.scale.z);
+        pt1 = new Vector3(pt1.x * this.scale.x, pt1.y * this.scale.y, pt1.z * this.scale.z);
+        pt2 = new Vector3(pt2.x * this.scale.x, pt2.y * this.scale.y, pt2.z * this.scale.z);
+        float v321 = pt2.x * pt1.y * pt0.z;
+        float v231 = pt1.x * pt2.y * pt0.z;
+        float v312 = pt2.x * pt0.y * pt1.z;
+        float v132 = pt0.x * pt2.y * pt1.z;
+        float v213 = pt1.x * pt0.y * pt2.z;
+        float v123 = pt0.x * pt1.y * pt2.z;
+        return (1.0f / 6.0f) * (-v321 + v231 + v312 - v132 - v213 + v123);
+    }
+    private float CalculateArea(Vector3 pt0, Vector3 pt1, Vector3 pt2)
+    {
+        pt0 = new Vector3(pt0.x * this.scale.x, pt0.y * this.scale.y, pt0.z * this.scale.z);
+        pt1 = new Vector3(pt1.x * this.scale.x, pt1.y * this.scale.y, pt1.z * this.scale.z);
+        pt2 = new Vector3(pt2.x * this.scale.x, pt2.y * this.scale.y, pt2.z * this.scale.z);
+        float a = (pt1 - pt0).magnitude;
+        float b = (pt2 - pt1).magnitude;
+        float c = (pt0 - pt2).magnitude;
+        float p = (a + b + c) * 0.5f;
+        return Mathf.Sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+    /* private void OnGUI()
+     {
+         GUILayout.Label("体积：" + Mathf.Abs(sum));
+         GUILayout.Label("面积：" +sum1);
+     }*/
+}
--- Assets/_iLYuSha Wakaka Setting/Scripts/Hangar/HangarRanger.cs
+++ Assets/_iLYuSha Wakaka Setting/Scripts/Hangar/HangarRanger.cs
@@ -4,83 +4,120 @@ using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
 
-public enum HangarState
+namespace Kocmoca
 {
-    Ready,
-    Moving,
-    Login,
-    Lobby,
-    Hangar,
-    Show,
-    newEvent,
-}
-public class HangarRanger : MonoBehaviour
-{
-    public Text textHangarInfo;
-    public Transform[] hangarCenter;
-    private Transform hangarCamera;
-    private Transform hangarRailX;
-    private Transform hangarRailY;
-    private int now = 0;
-    public HangarState hangarState = HangarState.Ready;
-    void Awake ()
-    {      
-        hangarCamera = transform;
-        hangarRailX = hangarCamera.parent.parent;
-        hangarRailY = hangarRailX.parent;
-
-        hangarState = HangarState.Moving;
-        hangarRailY.DOMove(hangarCenter[now].position, 0.73f).OnComplete(() =>
-        {
-            hangarState = HangarState.Ready;
-            textHangarInfo.text = Kocmoca.HangarData.OKB[now] + "\n" + Kocmoca.HangarData.Kocmocraft[now] + "\n" + Kocmoca.HangarData.Code[now] + "\n" + Kocmoca.HangarData.Dubi[now] + "\n拦截";
-        });
-        //hangarRailY.DORotateQuaternion(hangarCenter[now].rotation, 0.73f);
-    }	
-
-	void Update ()
+    public enum HangarState
     {
-        if (Input.GetKeyDown(KeyCode.D))
-            NextHanger();
-        else if (Input.GetKeyDown(KeyCode.A))
-            PreviousHanger();
+        Ready,
+        Moving,
+        Login,
+        Lobby,
+        Hangar,
+        Show,
+        newEvent,
+    }
+    public class HangarRanger : MonoBehaviour
+    {
+        [Header("Hangar Camera")]
+        public Transform hangarRailY;
+        public Transform hangarRailX;
+        public Transform hangarCamera;
+        public Transform[] hangarCenter;
+        private int now = 0;
+        public HangarState hangarState = HangarState.Ready;
 
-        if (hangarState == HangarState.Ready)
+        public Text textHangarInfo;
+        public Text textKocmocraft;
+        [Header("Kocmocraft Data")]
+        public Text textMaxHull;
+        public Text textMaxShield;
+        public Text textMaxEnergy;
+        public Text textCruiseSpeed;
+        public Text textAfterburneSpeed;
+        [Header("Weapon Data")]
+        public Text textTurretCount;
+        public Text textFireRPS;
+        public Text textDamage;
+        public Text textMaxRange;
+
+        void Awake()
         {
-            if (Input.GetKey(KeyCode.Mouse1))
+            hangarState = HangarState.Moving;
+            hangarRailY.DOMove(hangarCenter[now].position, 0.73f).OnComplete(() =>
             {
-                hangarRailY.rotation *= Quaternion.Euler(0,Input.GetAxis("Mouse X")*2,0);
-                hangarRailX.rotation *= Quaternion.Euler(-Input.GetAxis("Mouse Y")*2, 0,0);
-                hangarRailX.eulerAngles = new Vector3(Mathf.Clamp(hangarRailX.rotation.eulerAngles.x,60, 120), hangarRailX.rotation.eulerAngles.y, hangarRailX.rotation.eulerAngles.z);
-            }
-            hangarCamera.localPosition = Vector3.Lerp(hangarCamera.localPosition, hangarCamera.localPosition + new Vector3(0, 0, 10 * Input.GetAxis("Mouse ScrollWheel")), 0.5f);
-            hangarCamera.localPosition = new Vector3(hangarCamera.localPosition.x, hangarCamera.localPosition.y, Mathf.Clamp(hangarCamera.localPosition.z, -20, -5));
+                hangarState = HangarState.Ready;
+                LoadHangarData();
+            });
+            //hangarRailY.DORotateQuaternion(hangarCenter[now].rotation, 0.73f);
         }
-    }
 
-    void NextHanger()
-    {
-        now = (int)Mathf.Repeat(++now,hangarCenter.Length);
-        hangarRailY.DOKill();
-        hangarState = HangarState.Moving;
-        hangarRailY.DOMove(hangarCenter[now].position, 0.73f).OnComplete(()=>
+        void Update()
         {
-            hangarState = HangarState.Ready;
-            textHangarInfo.text = Kocmoca.HangarData.OKB[now] + "\n" + Kocmoca.HangarData.Kocmocraft[now] + "\n" + Kocmoca.HangarData.Code[now] + "\n" + Kocmoca.HangarData.Dubi[now] + "\n拦截";
-        });
-        //hangarRailY.DORotateQuaternion(hangarCenter[now].rotation, 0.73f);
-    }
+            if (Input.GetKeyDown(KeyCode.D))
+                NextHanger();
+            else if (Input.GetKeyDown(KeyCode.A))
+                PreviousHanger();
 
-    void PreviousHanger()
-    {
-        now = (int)Mathf.Repeat(--now, hangarCenter.Length);
-        hangarRailY.DOKill();
-        hangarState = HangarState.Moving;
-        hangarRailY.DOMove(hangarCenter[now].position, 0.73f).OnComplete(() =>
+            if (hangarState == HangarState.Ready)
+            {
+                if (Input.GetKey(KeyCode.Mouse1))
+                {
+                    hangarRailY.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * 2, 0);
+                    hangarRailX.rotation *= Quaternion.Euler(-Input.GetAxis("Mouse Y") * 2, 0, 0);
+                    hangarRailX.eulerAngles = new Vector3(Mathf.Clamp(hangarRailX.rotation.eulerAngles.x, 60, 120), hangarRailX.rotation.eulerAngles.y, hangarRailX.rotation.eulerAngles.z);
+                }
+                hangarCamera.localPosition = Vector3.Lerp(hangarCamera.localPosition, hangarCamera.localPosition + new Vector3(0, 0, 10 * Input.GetAxis("Mouse ScrollWheel")), 0.5f);
+                hangarCamera.localPosition = new Vector3(hangarCamera.localPosition.x, hangarCamera.localPosition.y, Mathf.Clamp(hangarCamera.localPosition.z, -20, -5));
+            }
+        }
+
+        void NextHanger()
         {
-            hangarState = HangarState.Ready;
-            textHangarInfo.text = Kocmoca.HangarData.OKB[now] + "\n" + Kocmoca.HangarData.Kocmocraft[now] + "\n" + Kocmoca.HangarData.Code[now] + "\n" + Kocmoca.HangarData.Dubi[now] + "\n拦截";
-        });
-        //hangarRailY.DORotateQuaternion(hangarCenter[now].rotation, 0.73f);
+            now = (int)Mathf.Repeat(++now, hangarCenter.Length);
+            hangarRailY.DOKill();
+            hangarState = HangarState.Moving;
+            hangarRailY.DOMove(hangarCenter[now].position, 0.73f).OnComplete(() =>
+            {
+                hangarState = HangarState.Ready;
+                LoadHangarData();
+            });
+            //hangarRailY.DORotateQuaternion(hangarCenter[now].rotation, 0.73f);
+        }
+
+        void PreviousHanger()
+        {
+            now = (int)Mathf.Repeat(--now, hangarCenter.Length);
+            hangarRailY.DOKill();
+            hangarState = HangarState.Moving;
+            hangarRailY.DOMove(hangarCenter[now].position, 0.73f).OnComplete(() =>
+            {
+                hangarState = HangarState.Ready;
+                LoadHangarData();
+            });
+            //hangarRailY.DORotateQuaternion(hangarCenter[now].rotation, 0.73f);
+        }
+
+        void LoadHangarData()
+        {
+            // General
+            textHangarInfo.text = HangarData.OKB[now] + "\n" + HangarData.Kocmocraft[now] + "\n" + HangarData.Code[now] + "\n" + HangarData.Dubi[now] + "\n拦截";
+            // 
+            textKocmocraft.text =
+                KocmocraftData.MaxHull[now] + "\n" + 
+                KocmocraftData.MaxShieldl[now] + "\n" + 
+                KocmocraftData.MaxEnergy[now] + "\n" + 
+                KocmocraftData.CruiseSpeed[now] * 3.6f + " km/h\n" + 
+                KocmocraftData.AfterburnerSpeed[now] * 3.6f + " km/h";
+
+            //textMaxHull.text = "" + KocmocraftData.MaxHull[now];
+            //textMaxShield.text = "" + KocmocraftData.MaxShieldl[now];
+            //textMaxEnergy.text = "" + KocmocraftData.MaxEnergy[now];
+            //textCruiseSpeed.text = "" + KocmocraftData.CruiseSpeed[now] * 3.6f + " km/h";
+            //textAfterburneSpeed.text = "" + KocmocraftData.AfterburnerSpeed[now] * 3.6f + " km/h";
+            //textTurretCount.text = "" + KocmocraftData.GetTurretCount((Type)now) + "x Assault Laser";
+            //textFireRPS.text = "" + KocmoLaserCannon.fireRoundPerSecond + " rps";
+            //textDamage.text = "" + KocmocraftData.GetPowerData((Type)now) + " dmg";
+            //textMaxRange.text = "" + KocmocraftData.GetMaxRangeData((Type)now) + " m"; 
+        }
     }
 }
